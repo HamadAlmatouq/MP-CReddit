20a1b3fc1b29f86dab1944c309bf2fcd3a599ec9
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-11-11 13:07:53 +0300

    MP-CReddit V0.3

--- api/posts/controller.js
+++ api/posts/controller.js
@@ -1,6 +1,7 @@
 const uuid4 = require("uuid4");
 const posts = require("../../posts");
 const { title } = require("process");
+const { type } = require("os");
 
 //GET
 const postDetail = (request, response) => {
@@ -9,6 +10,7 @@ const postDetail = (request, response) => {
   response.status(200).json(foundPost);
 };
 
+//GET By title or ID
 const postList = (request, response) => {
   const { title, id } = request.query;
 
@@ -17,6 +19,7 @@ const postList = (request, response) => {
     filteredPosts = posts.filter((post) =>
       post.title.toLowerCase().includes(title.toLowerCase())
     );
+
   if (id)
     filteredPosts = posts.filter((post) =>
       post.id.toLowerCase().includes(id.toLowerCase())
@@ -35,15 +38,44 @@ const postList = (request, response) => {
 };
 
 const createPost = (request, response) => {
+  const { title, description } = request.body;
+
   const newPost = {
-    ...request.body,
     id: uuid4(),
     title: title,
     description: description,
+    // ...request.body,
+    // comments: [],
   };
 
+  if (!title) response.status(400).json({ message: "Missing title!" });
+  else if (typeof title !== "string")
+    response.status(400).json({ message: "Title must be String" });
+  else if (!description)
+    response.status(400).json({ message: "Missing description!" });
+  else if (typeof description !== "string")
+    response.status(400).json({ message: "description must be String" });
+  // else {
   posts.unshift(newPost);
   response.status(201).json(newPost);
+  // }
+};
+
+const addComment = (req, res) => {
+  const { postId } = req.params;
+  const { username, comment } = req.body;
+
+  if ((!username, !comment)) {
+    return res
+      .status(400)
+      .json({ message: "Both username and comment are required" });
+  }
+  const post = posts.find((p) => p.id === postId);
+  if (!post) return res.status(404).json({ message: "Post not found" });
+
+  const newComment = { id: uuid4(), username, comment };
+  post.comments.push(newComment);
+  res.status(201).json(newComment);
 };
 
 //Delete
@@ -55,7 +87,19 @@ const deletePost = (request, response) => {
     (post) => post.id === request.params.postsId
   );
   posts.splice(postIndex, 1);
-  response.status(204).end();
+  response.status(204).json({ message: "Post deleted!" });
+};
+
+const deleteCommentById = (req, res) => {
+  const commentId = req.params.id;
+  for (const post of posts) {
+    const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+    if (commentIndex !== -1) {
+      post.comments.splice(commentIndex, 1);
+      return res.status(200).json({ message: "Comment deleted!" });
+    }
+  }
+  res.status(404).json({ message: "No comment found!" });
 };
 
 //Put / update
@@ -79,6 +123,8 @@ module.exports = {
   postList,
   postDetail,
   //   updatePost,
+  addComment,
+  deleteCommentById,
   deletePost,
   createPost,
 };
--- api/posts/routers.js
+++ api/posts/routers.js
@@ -3,15 +3,19 @@ const {
   postList,
   postDetail,
   //   updatePost,
+  deleteCommentById,
+  addComment,
   deletePost,
   createPost,
 } = require("./controller");
 const router = express.Router();
 
 router.get("/:postId", postDetail);
-router.get("/", postList);
-router.post("/", createPost);
+router.get("/postList", postList);
+router.post("/createPost", createPost);
 // router.put("/:accountsId", updatePost);
-router.delete("/:postId", deletePost);
+router.post("/", addComment);
+router.delete("/:postId/comments", deletePost);
+router.delete("/:postId/:id", deleteCommentById);
 
 module.exports = router;
